bc73f00d85d397690c6239e8b5c2ce29d95a7c43
bao <camhoangbao@gmail.com>
2018-09-20 16:22:45 +0700

    Finish Sinhvien

---
+++ src/bao/dev/Sinhvien.java
@@ -0,0 +1,17 @@
+package bao.dev;
+
+public class Sinhvien {
+	private String name;
+	private double toan;
+	private double ly;
+	private double hoa;
+		
+	public Sinhvien(String name, double toan, double ly, double hoa) {	
+		
+	this.name = name;
+	this.toan = toan;
+	this.ly = ly;
+	this.hoa = hoa;
+	}
+}
+
